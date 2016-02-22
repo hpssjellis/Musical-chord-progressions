317c19a80ca41dcb7dbc74f8247d206afee736af
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-22 10:19:53 -0800

    Update cloud9-intstall.sh

--- cloud9-intstall.sh
+++ cloud9-intstall.sh
@@ -6,11 +6,20 @@
 # best in cloud 9 to just right click this file and select run
 #from https://www.digitalocean.com/community/tutorials/how-to-set-up-rstudio-on-an-ubuntu-cloud-server
 
+
+sudo apt-get update
+
 sudo apt-get install r-base libapparmor1 gdebi-core
 
 wget http://download2.rstudio.org/rstudio-server-0.97.336-amd64.deb -O rstudio.deb
 
 sudo gdebi rstudio.deb
+
+
+
+echo "Enter a password to use in R-studio and then hit enter for defaults"
+
+sudo adduser rstudio
 
 
 mkdir /etc/rstudio/rserver.conf
@@ -19,12 +28,6 @@ printf "\n\n>www-port=$PORT\nwww-address=$ID"  >> /etc/rstudio/rserver.conf
 
 
 ln -s /usr/lib/rstudio-server /home/ubuntu/workspace/rstudio-link
-
-
-echo "Enter a password to use in R-studio and then hit enter for defaults"
-
-sudo adduser rstudio
-
 
 
 # inside rstudio run
