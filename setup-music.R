f5953dc965af9f4ef0a968c824a760c6b87676c2
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-23 10:04:19 -0800

    Update setup-music.R

--- setup-music.R
+++ setup-music.R
@@ -4,9 +4,33 @@
 
 
 
-install.packages("rjson_0.2.13.tar.gz", repos=NULL, type="source")
+#install.packages("rjson_0.2.13.tar.gz", repos=NULL, type="source")
 #install.packages('rjson', repos = 'http://cran.us.r-project.org')
-install.packages('reshape2', repos = 'http://cran.us.r-project.org')
+#install.packages('reshape2', repos = 'http://cran.us.r-project.org')
 install.packages('googleVis', repos = 'http://cran.us.r-project.org')
+
+
+
+#from http://hadley.nz/
+
+
+
+install.packages('ggplot2', repos = 'http://cran.us.r-project.org')
+install.packages('dplyr', repos = 'http://cran.us.r-project.org')
+install.packages('tidyr', repos = 'http://cran.us.r-project.org')
+install.packages('stringr', repos = 'http://cran.us.r-project.org')
+install.packages('lubridate', repos = 'http://cran.us.r-project.org')
+install.packages('readr', repos = 'http://cran.us.r-project.org')
+install.packages('readxl', repos = 'http://cran.us.r-project.org')
+install.packages('haven', repos = 'http://cran.us.r-project.org')
+install.packages('httr', repos = 'http://cran.us.r-project.org')
+install.packages('rvest', repos = 'http://cran.us.r-project.org')
+install.packages('xml2', repos = 'http://cran.us.r-project.org')
+install.packages('devtools', repos = 'http://cran.us.r-project.org')
+install.packages('roxygen2', repos = 'http://cran.us.r-project.org')
+install.packages('testthat', repos = 'http://cran.us.r-project.org')
+
+
+
 
 
