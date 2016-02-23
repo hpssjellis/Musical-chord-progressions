39779f2978ac09271ec57d74b1e3390a084d26ca
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-22 20:27:22 -0800

    Create Chords-install.R

---
+++ Chords-install.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+
+
+
+
+install.packages("rjson_0.2.13.tar.gz", repos=NULL, type="source")
+#install.packages('rjson', repos = 'http://cran.us.r-project.org')
+install.packages('reshape2', repos = 'http://cran.us.r-project.org')
+install.packages('googleVis', repos = 'http://cran.us.r-project.org')
+
+
